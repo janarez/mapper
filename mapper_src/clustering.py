713dcf1d7c56fee33bbbabcd2f1a41a95d829d6d
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-01-08 19:11:54 +0100

    Add tomato and davies bouldin

--- mapper_src/clustering.py
+++ mapper_src/clustering.py
@@ -1,18 +1,22 @@
 import operator
+import sys
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering, DBSCAN
+from sklearn.metrics import davies_bouldin_score
+from gudhi.clustering.tomato import Tomato
 
 class Clustering:
     """
     Function clustering vertices to nodes.
     Returns list of node indices for each vertex.
     """
-    def __init__(self, clustering_function, **kwargs):
+    def __init__(self, clustering_function='agglomerative', **kwargs):
+        self._clustering_function = clustering_function
         if clustering_function == 'agglomerative':
             self._clustering = AgglomerativeClustering(
                 n_clusters=None,
                 compute_full_tree=True,
-                linkage=kwargs.get('linkage', 'ward')
+                linkage=kwargs.get('linkage', 'ward'),
             )
             self._distance_param = 'distance_threshold'
 
@@ -20,12 +24,63 @@ class Clustering:
             self._clustering = DBSCAN()
             self._distance_param = 'eps'
 
+        elif clustering_function == 'tomato':
+            self._show_diagram = kwargs.get('cluster_plot', False)
         else:
-            raise ValueError(f'Argument `clustering_function` must be one of: "agglomerative", "DBSCAN".')
-      
-    def __call__(self, vertices, indices, distance):
-        self._clustering.set_params(**{self._distance_param : distance})
-        labels = self._clustering.fit_predict(vertices)
+            raise ValueError(f'Argument `clustering_function` must be one of: "agglomerative", "DBSCAN", "tomato".')
+
+        # If not None clustering is only run for this single value.
+        self._distance = kwargs.get('distance', None)
+        # Else clusters up to `max_k` are tested.
+        self._max_k = kwargs.get('max_k', 10)
+
+
+    def __call__(self, vertices, indices):
+        if self._clustering_function == 'tomato':
+            return self._gudhi_clustering(vertices, indices)
+        else:
+            return self._scikit_clustering(vertices, indices)
+
+
+    def _gudhi_clustering(self, vertices, indices):
+        # We must recreate the object as it saves fitting information from past fits.
+        self._clustering = Tomato(merge_threshold=self._distance)
+        self._clustering.fit(vertices)
+
+        if self._show_diagram:
+            self._clustering.plot_diagram()
+            n_clusters = int(input('Set optimal number of clusters: '))
+            self._clustering.n_clusters_ = n_clusters
+
+        return self._assign_vertices_to_clusters(vertices, indices)
+
+
+    def _scikit_clustering(self, vertices, indices):
+        self._clustering.set_params(**{self._distance_param : self._distance})
+
+        if self._distance is None:
+            # Find optimal number of clusters by DB score.
+            opt_db = sys.maxsize
+            opt_k = 1
+            for k in range(2, self._max_k+1):
+                self._clustering.n_clusters = k
+                labels = self._clustering.fit_predict(vertices)
+                db_score = davies_bouldin_score(vertices, labels)
+
+                if db_score < opt_db:
+                    opt_db = db_score
+                    opt_k = k
+
+            self._clustering.n_clusters = opt_k
+            self._clustering.fit(vertices)
+        else:
+            self._clustering.fit(vertices)
+
+        return self._assign_vertices_to_clusters(vertices, indices)
+
+
+    def _assign_vertices_to_clusters(self, vertices, indices):
+        labels = self._clustering.labels_
 
         label_indices = [set() for _ in range(self._clustering.n_clusters_)]
         cluster_vertices = [list() for _ in range(self._clustering.n_clusters_)]
@@ -35,6 +90,6 @@ class Clustering:
             cluster_vertices[l].append(vertices[index])
 
         # Calculate center of each cluster.
-        cluster_centers = [(np.mean(np.array(c)[:, 0]), np.mean(np.array(c)[:, 1])) for c in cluster_vertices]
+        cluster_centers = [(np.mean(np.array(c)[:, 0]), np.mean(np.array(c)[:, 1]), np.mean(np.array(c)[:, 2])) for c in cluster_vertices]
 
         return labels, label_indices, cluster_centers

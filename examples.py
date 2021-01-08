874016d75dc2ae2bd775b000abb60051dcad36f9
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-01-08 19:10:38 +0100

    Finish all 3d plotting variants

--- examples.py
+++ examples.py
@@ -24,12 +24,18 @@ with open('pliers.txt') as f:
     data = f.readlines()
 
 points = np.array([list(map(float, p.strip().split(' '))) for p in data])
+#points_z = np.array([[p[0], p[2], p[1]] for p in points])
 
-
-mapper = Mapper(distance=200, linkage="average")
+mapper = Mapper(
+    bins=5,
+    clustering_function="agglomerative",
+    linkage="average",
+    coordinate=-1,
+    cluster_plot=False,
+    max_k=5)
 
 graph = mapper.fit(points)
 mapper.plot_vertices_3d()
 mapper.plot_intervals_3d()
-# mapper.plot_clusters()
-mapper.plot_graph()
+mapper.plot_clusters_3d()
+mapper.plot_graph_3d()
--- mapper_src/mapper.py
+++ mapper_src/mapper.py
@@ -11,10 +11,13 @@ from mapper_src.cover import Cover
 
 class Mapper:
     def __init__(self, **kwargs):
-        self.filter = Filter(kwargs.get('filter_function', 'last_coordinate'))
-        self.cover = Cover(kwargs.get('cover_function', 'linear'), **kwargs)
-        self.clustering = Clustering(kwargs.get('clustering_function', 'agglomerative'))
-        self._clustering_distance = kwargs.get('distance', 2)
+        self.filter = Filter(**kwargs)
+        self.cover = Cover(**kwargs)
+        self.clustering = Clustering(**kwargs)
+
+        # Coordinate for subplot splitting.
+        self._coordinate = kwargs.get('coordinate', -1)
+
 
     def fit(self, vertices):
         """
@@ -26,12 +29,14 @@ class Mapper:
         self.intervals = self.cover(self.numbers)
         self.n_intervals = len(self.intervals)
 
+        self._initialise_plotting()
+
         # Sort intervals and numbers so that we assign them in order into partitions.
         self.numbers, self.vertices = zip(*sorted(zip(self.numbers, self.vertices), key=lambda t: t[0]))
         self.intervals.sort(key=lambda t: (t[0], t[1]))
         self._assign_partitions()
 
-        # Collect all clusters - a.k.a graph nodes 
+        # Collect all clusters - a.k.a graph nodes
         # and their intersections - a.k.a graph edges.
         node_index = 0
         self.nodes = {}
@@ -40,10 +45,10 @@ class Mapper:
         prev_node_index = 0
 
         for indices in self.partitions.values():
-            
+
             # Cluster vertices in this partion.
             convert_indices = operator.itemgetter(*indices)
-            _, interval_clusters, cluster_centers = self.clustering(convert_indices(self.vertices), indices, self._clustering_distance)
+            _, interval_clusters, cluster_centers = self.clustering(convert_indices(self.vertices), indices)
 
             # Find connections.
             for cluster_a, cluster_a_center in zip(interval_clusters, cluster_centers):
@@ -59,11 +64,11 @@ class Mapper:
             prev_node_index = node_index - len(interval_clusters)
 
         return self.nodes
-        
+
     def _assign_partitions(self):
         "Distributes vertices to cover partitions given by intervals."
         self.partitions = defaultdict(set)
-        
+
         min_start = 0
         unused = 0
 
@@ -78,23 +83,39 @@ class Mapper:
                 elif b < n:
                     unused += 1 # Too low.
 
+    def _initialise_plotting(self):
+        self._plot_box_aspect = (
+            np.ptp(np.array(self.vertices)[:, 0]),
+            np.ptp(np.array(self.vertices)[:, 1]),
+            np.ptp(np.array(self.vertices)[:, 2])
+        )
+
+        self._plot_lim = [
+            (np.min(np.array(self.vertices)[:, 0]), np.max(np.array(self.vertices)[:, 0])),
+            (np.min(np.array(self.vertices)[:, 1]), np.max(np.array(self.vertices)[:, 1])),
+            (np.min(np.array(self.vertices)[:, 2]), np.max(np.array(self.vertices)[:, 2]))
+        ]
+
+    def _limit_axis(self, ax):
+        if self._coordinate != 0:
+            ax.set_xlim3d(*self._plot_lim[0])
+        if self._coordinate != 1:
+            ax.set_ylim3d(*self._plot_lim[1])
+        if self._coordinate != 2 or self._coordinate != -1:
+            ax.set_ylim3d(*self._plot_lim[2])
+
     def plot_vertices_3d(self):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         sc = ax.scatter(np.array(self.vertices)[:, 0], np.array(self.vertices)[:, 1], np.array(self.vertices)[:, 2], c=self.numbers)
-        ax.set_box_aspect(
-            (
-                np.ptp(np.array(self.vertices)[:, 0]), 
-                np.ptp(np.array(self.vertices)[:, 1]), 
-                np.ptp(np.array(self.vertices)[:, 2])
-            )
-        )
+        ax.set_box_aspect(self._plot_box_aspect)
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
         plt.colorbar(sc)
         plt.show()
+
 
     def plot_vertices(self):
         fig = plt.figure()
@@ -104,6 +125,7 @@ class Mapper:
         fig.suptitle(f'Vertices ({len(self.vertices)})')
         plt.colorbar(sc)
         plt.show()
+
 
     def plot_intervals_3d(self):
         fig = plt.figure(figsize=plt.figaspect(self.n_intervals))
@@ -116,24 +138,21 @@ class Mapper:
             interval_numbers = convert_indices(self.numbers)
 
             ax.scatter(
-                np.array(interval_vertices)[:, 0], 
-                np.array(interval_vertices)[:, 1], 
-                np.array(interval_vertices)[:, 2], 
+                np.array(interval_vertices)[:, 0],
+                np.array(interval_vertices)[:, 1],
+                np.array(interval_vertices)[:, 2],
                 c=interval_numbers,
                 norm=n
             )
-        
-            ax.set_box_aspect((
-                np.ptp(np.array(self.vertices)[:, 0]), 
-                np.ptp(np.array(self.vertices)[:, 1]), 
-                np.ptp(np.array(self.vertices)[:, 2])
-            ))
 
+            self._limit_axis(ax)
+            ax.set_box_aspect(self._plot_box_aspect)
             ax.set_xlabel('X')
             ax.set_ylabel('Y')
             ax.set_zlabel('Z')
 
         plt.show()
+
 
     def plot_intervals(self):
         fig = plt.figure()
@@ -145,7 +164,7 @@ class Mapper:
             interval_numbers = convert_indices(self.numbers)
 
             ax[self.n_intervals-i-1].scatter(
-                np.array(interval_vertices)[:, 0], 
+                np.array(interval_vertices)[:, 0],
                 np.array(interval_vertices)[:, 1],
                 c=interval_numbers,
                 norm=n
@@ -153,24 +172,72 @@ class Mapper:
         fig.suptitle(f'Intervals ({self.n_intervals})')
         plt.show()
 
+    def plot_clusters_3d(self):
+        fig = plt.figure(figsize=plt.figaspect(self.n_intervals))
+        cluster_count = 0
+
+        for i, indices in self.partitions.items():
+            ax = fig.add_subplot(self.n_intervals, 1, self.n_intervals-i, projection='3d')
+
+            convert_indices = operator.itemgetter(*indices)
+            interval_vertices = convert_indices(self.vertices)
+            labels, _, cluster_centers = self.clustering(interval_vertices, indices)
+
+            # ax.scatter(
+            #     np.array(interval_vertices)[:, 0],
+            #     np.array(interval_vertices)[:, 1],
+            #     np.array(interval_vertices)[:, 2],
+            #     c=labels
+            # )
+            ax.scatter(*zip(*cluster_centers), 'ro')
+
+            ax.set_box_aspect(self._plot_box_aspect)
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+
+            cluster_count += len(cluster_centers)
+
+        fig.suptitle(f'Clusters {cluster_count}')
+        plt.show()
+
+
     def plot_clusters(self):
         fig = plt.figure()
         ax = fig.subplots(self.n_intervals, sharex=True)
+        cluster_count = 0
 
         for i, indices in self.partitions.items():
             convert_indices = operator.itemgetter(*indices)
             interval_vertices = convert_indices(self.vertices)
-            labels, _, cluster_centers = self.clustering(interval_vertices, indices, self._clustering_distance)
+            labels, _, cluster_centers = self.clustering(interval_vertices, indices)
 
             ax[self.n_intervals-i-1].scatter(
-                np.array(interval_vertices)[:, 0], 
+                np.array(interval_vertices)[:, 0],
                 np.array(interval_vertices)[:, 1],
                 c=labels
             )
             ax[self.n_intervals-i-1].plot(*zip(*cluster_centers), 'ro', markersize=10)
+            cluster_count += len(cluster_centers)
 
-        fig.suptitle(f'Clusters (COUNT HERE)')
+        fig.suptitle(f'Clusters {cluster_count}')
         plt.show()
+
+
+    def plot_graph_3d(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(*zip(*self.node_vertices))
+        ax.set_box_aspect(
+
+        )
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        fig.suptitle(f'Mapper [V = {len(self.node_vertices)}, E = {sum([len(e) for e in self.node_vertices])}]')
+        plt.show()
+
 
     def plot_graph(self):
         fig, ax = plt.subplots()
@@ -186,7 +253,7 @@ class Mapper:
                 plt.plot(
                     [vertex_a[0], vertex_b[0]],
                     [vertex_a[1], vertex_b[1]],
-                    color='b', 
+                    color='b',
                     linewidth=2,
                     zorder=0
                 )

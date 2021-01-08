96e5730064f92b1f251946b47a494a81e7d9fb16
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-01-08 19:11:05 +0100

    Allow filtering by any coordinate

--- mapper_src/filter.py
+++ mapper_src/filter.py
@@ -2,17 +2,19 @@ class Filter:
     """
     Implements function filter: X -> R.
     """
-    def __init__(self, filter_function):
+    def __init__(self, filter_function='by_coordinate', **kwargs):
         self._filter_function = filter_function
+        if filter_function == 'by_coordinate':
+            self._coordinate = kwargs.get('coordinate', -1)
 
     def __call__(self, vertices):
-        if self._filter_function == 'last_coordinate':
-            return self._last_coordinate(vertices)
+        if self._filter_function == 'by_coordinate':
+            return self._by_coordinate(vertices)
         else:
-            raise ValueError(f'Argument `filter_function` must be one of: "last_coordinate".')
+            raise ValueError(f'Argument `filter_function` must be one of: "by_coordinate".')
 
-    def _last_coordinate(self, vertices):
+    def _by_coordinate(self, vertices):
         """
         Uses last coordinate of vertices.
         """
-        return vertices[:, -1]
+        return vertices[:, self._coordinate]

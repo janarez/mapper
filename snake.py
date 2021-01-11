9bdc8aeae87ec65344ad48248227029a6fce3e7f
Jan Joneš <jjones@outlook.cz>
2021-01-11 11:11:27 +0100

    Add snake

---
+++ snake.py
@@ -0,0 +1,21 @@
+import numpy as np
+from mapper_src.mapper import Mapper
+
+
+with open('snake.txt') as f:
+    data = f.readlines()
+
+points = np.array([list(map(float, p.strip().split(' '))) for p in data])
+
+
+mapper = Mapper(
+    clustering_function='tomato',
+    distance=200
+)
+
+graph = mapper.fit(points)
+mapper.plot_vertices_3d()
+mapper.plot_intervals_3d()
+mapper.plot_clusters_3d()
+mapper.plot_graph_3d()
+mapper.plot_persistence_homology()

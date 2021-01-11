b8134d00694cd9440939d6d31d5d3ff4b2e3ed9f
Jan Joneš <jjones@outlook.cz>
2021-01-11 11:04:50 +0100

    Add lion

---
+++ lion.py
@@ -0,0 +1,21 @@
+import numpy as np
+from mapper_src.mapper import Mapper
+
+
+points = np.load('lion.npy')
+
+
+mapper = Mapper(
+    bins=7,
+    overlap=0.1,
+    clustering_function='tomato',
+    distance=6,
+    filter_function='distance_from_origin'
+)
+
+graph = mapper.fit(points)
+mapper.plot_vertices_3d()
+mapper.plot_intervals_3d()
+mapper.plot_clusters_3d()
+mapper.plot_graph_3d()
+mapper.plot_persistence_homology()

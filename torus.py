4db232cb0e1f3a12b74f7f856b3790b82b7600e4
Jan Joneš <jjones@outlook.cz>
2021-01-11 11:30:33 +0100

    Use Silhouette score

--- torus.py
+++ torus.py
@@ -31,3 +31,19 @@ mapper.plot_intervals_3d()
 mapper.plot_clusters_3d()
 mapper.plot_graph_3d()
 mapper.plot_persistence_homology()
+
+# Silhouette score
+mapper = Mapper(
+    bins=4,
+    filter_function='by_coordinate',
+    coordinate=1,
+    clustering_function='agglomerative',
+    linkage="average"
+)
+
+graph = mapper.fit(points)
+mapper.plot_vertices_3d()
+mapper.plot_intervals_3d()
+mapper.plot_clusters_3d()
+mapper.plot_graph_3d()
+mapper.plot_persistence_homology()

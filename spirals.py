d96dca639e3e8c00dac81e31aa9af65128cd86c4
Jan Joneš <jjones@outlook.cz>
2021-01-11 10:41:50 +0100

    Add spirals

---
+++ spirals.py
@@ -0,0 +1,21 @@
+import numpy as np
+from mapper_src.mapper import Mapper
+
+
+points = np.load('spirals.npy')
+
+
+mapper = Mapper(
+    bins=7,
+    overlap=0.04,
+    clustering_function='tomato',
+    distance=200,
+    coordinate=0
+)
+
+graph = mapper.fit(points)
+mapper.plot_vertices_3d()
+mapper.plot_intervals_3d()
+mapper.plot_clusters_3d()
+mapper.plot_graph_3d()
+mapper.plot_persistence_homology()
